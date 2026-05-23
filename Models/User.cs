5aa398a41beb711111898354acdc8b5986ab5037
Marina <marina.khodova.06@mail.ru>
2026-05-23 17:02:03 +0300

    Добавила класс User, как его интегрировать тотально не знаю

---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿namespace WriterApp.Models
+{
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public string NickName { get; set; }
+        public string Email { get; set; }
+        public List<Collection> Collections { get; set; }
+
+        public User(string nickName, string email)
+        {
+            UserId = Guid.NewGuid();
+            NickName = nickName;
+            Email = email;
+            Collections = new List<Collection>()
+            {
+                new Collection(0,"В процессе"),
+                new Collection(1,"Завершено")
+            };
+
+        }
+    }
+}
